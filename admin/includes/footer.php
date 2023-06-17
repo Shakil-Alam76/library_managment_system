394acdcf2e974e8643bdd27e180f60e184906952
Md.Shakil Alam <shakil.shanto76@gmail.com>
2023-06-17 11:46:45 +0600

    final presentation

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+     
+    </div>
+    <strong>&copy; 2023 - Library Management System | Design and Developed By "Shakil, Sagir & Jahid" - CSE 06 Batch</strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Open Admin Panel and Add/View user to know Login ID</b>
+      </div>
+      <strong>&copy; 2023 - Library Management System | Design and Developed By "Shakil, Sagir & Jahid" - CSE 06 Batch</strong>
+      </div>
+    <!-- /.container -->
+</footer>
